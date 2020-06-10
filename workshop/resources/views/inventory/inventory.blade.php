f8a84e0c18ec178ed2f579bef83fd2939d3ddaa1
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-06-10 16:39:53 +0200

    blade inv

--- workshop/resources/views/inventory/inventory.blade.php
+++ workshop/resources/views/inventory/inventory.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 @section('content')
+    <div style="display: flex">
 
     @foreach($pokemons as $pokemon)
         <div>
-            <p>{{$pokemon->name}}</p>
+            <p>{{ ucfirst($pokemon->name) }}</p>
+            <p>Level: {{ $pokemon->level }}</p>
             <img src="{{ $pokemon->sprite }}" alt="">
         </div>
 
     @endforeach
+    </div>
 
 @endsection
 
