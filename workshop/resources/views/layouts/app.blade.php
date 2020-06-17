2cc60953f9d8ee45a7d063bcc42613f1ea35a5da
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-06-17 15:26:16 +0200

    merge

--- workshop/resources/views/layouts/app.blade.php
+++ workshop/resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('resource/css/style.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
